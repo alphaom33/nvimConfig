7ca7aca1b684ad18b95470d99855f38886356f7e
alphaom33 <alphaom34@gmail.com>
2024-10-28 18:08:13 -0500

    please

--- lua/2PBHD/change-accounts.lua
+++ lua/2PBHD/change-accounts.lua
@@ -2,10 +2,12 @@ function SetAccount(name)
 	vim.cmd.let('$GH_CONFIG_DIR = "' .. name  .. '"')
 end
 
-vim.keymap.set("n", "<C-A>", function ()
+local modes = {"n"}
+
+vim.keymap.set(modes, "<C-A>", function ()
 	SetAccount(".alphaom33")
 end)
 
-vim.keymap.set("n", "<C-M>", function ()
+vim.keymap.set(modes, "<C-M>", function ()
 	SetAccount(".mborchert")
 end)
